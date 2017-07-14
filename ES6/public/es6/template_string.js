69f7374c564b69b43b4e16a4ad44defbc7a7aabd
GoDonghyun <godong9@gmail.com>
2017-07-15 06:49:23 +0900

    Add variable_and_const, template_string, symbol

---
+++ ES6/public/es6/template_string.js
@@ -0,0 +1,3 @@
+let currentTemp = 19.5;
+const message = `The current temperature is ${currentTemp}\u00b0C`;
+console.log(message);
---
+++ ES6/public/es6/variable_and_const.js
@@ -0,0 +1,6 @@
+let currentTempC = 22;
+
+const ROOM_TEMP_C = 21.5;
+
+console.log(currentTempC, ROOM_TEMP_C);
+
